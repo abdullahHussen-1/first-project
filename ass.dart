d6eda33608f7599b5744e86b0b1c00015a9c4541
abdullahHussen-1 <abdullahhussein.04a@gmail.com>
2025-03-02 00:27:25 +0200

    Update ass.dart
    /*
    /** 1.1.Create a Book class with properties like title and author. Implement a copy constructor that initializes a new book object using another book object. 
    1.2 Create 10 Books about author : Dostoevsky*/
    
    void main (){
    //task1
     Book b1=Book(title: "Crime and Punishment ", author: "Dostoevsky");
     Book b2=Book(title: "The Idiot ", author: "Dostoevsky");
     Book b3=Book(title: "Demons ", author: "Dostoevsky");
     Book b4=Book(title: "The Brothers Karamazov ", author: "Dostoevsky");
     Book b5=Book(title: "Notes from Underground ", author: "Dostoevsky");
     Book b6=Book(title: "The Gambler ", author: "Dostoevsky");
     Book b7=Book(title: "Poor Folk ", author: "Dostoevsky");
     Book b8=Book(title: "White Nights ", author: "Dostoevsky");
     Book b9=Book(title: "The Eternal Husband ", author: "Dostoevsky");
     Book b10=Book(title: "The Insulted and Humiliated ", author: "Dostoevsky");
    
      
    }
    
    class Book{
      String? title;
      String?author;
      Book({required String title,required String author }){
        this.title=title;
        this.author=author;
      }
    }*/
    
    
    /*
    ///task2
    
    /*2.1.Define a BankAccount class with properties like id , expiredate , year ,cash
    2.2 Create 3 VIP clients in this bank (the cash of his/her acoount more than 1 milion)
    2.3 when call AccountDetails (method) return  that the account is vip or not ?*/
    void main(){
      
      BankAccount cl1=BankAccount(id: "1", expiredate: "12/2027", year: 30, cash: 1000000);
      BankAccount cl2=BankAccount(id: "2", expiredate: "10/2030", year: 35, cash: 100000);
      BankAccount cl3=BankAccount(id: "33", expiredate: "8/2035", year: 25, cash: 10000000);
    
    
    
     print(cl1.AccountDetails());
     print(cl2.AccountDetails());
     print(cl3.AccountDetails());
    }
    ///class
    class BankAccount {
    
      ///properties
      String? id;
      String?expiredate;
      int?year;
      double?cash;
    
      ///constructor
      BankAccount({required this.id,required this.expiredate,required this.year,required this.cash});
       
    
       ///method
       String AccountDetails ()
       {
        if (cash!>1000000)
        {
          return "The client is VIP";
    
        }
        else{
          return"The client is Not VIP";
        }
       }
       
    }
    */
    
    /*
    
    /*task3
    . Extend the TrafficLight enum to store a duration for each light 
     3.2 Modify the TrafficLight enum and create a class TrafficSystem with a method getNextLight(TrafficLight current) that returns the next light based on the current one*/
    
     void main(){
      TrafficLight light=TrafficLight.red;
       dynamic c =10;
      while(true)
      {
      switch(light){
        case TrafficLight.red:
        print("Yellow");
        light=TrafficLight.yellow;
        break;
        case TrafficLight.yellow:
         print("Green");
         light=TrafficLight.green;
         break;
        case TrafficLight.green:
          print("Red");
          light=TrafficLight.red;
          break;
          
      }
      c--;
      if(c<=0)
      {
        break;
      }
      }
    
     }
    
    enum TrafficLight {red,yellow,green}
    */

--- ass.dart
+++ ass.dart
@@ -190,7 +190,7 @@ void main(){
 قم بكتابة دالة لطباعة قائمة الطلاب الذين حصلوا على متوسط درجات أعلى من قيمة معينة.
 استخدم العمليات الحسابية والمنطقية والشروط للقيام بالعمليات المطلوبة.
 */
-
+/*
 void main(){
     Map<String,List<int>> students={
 
@@ -214,4 +214,124 @@ void main(){
         sum=0;
       });
 
+}*/
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+///task2
+
+    /*
+/** 1.1.Create a Book class with properties like title and author. Implement a copy constructor that initializes a new book object using another book object. 
+1.2 Create 10 Books about author : Dostoevsky*/
+
+void main (){
+//task1
+ Book b1=Book(title: "Crime and Punishment ", author: "Dostoevsky");
+ Book b2=Book(title: "The Idiot ", author: "Dostoevsky");
+ Book b3=Book(title: "Demons ", author: "Dostoevsky");
+ Book b4=Book(title: "The Brothers Karamazov ", author: "Dostoevsky");
+ Book b5=Book(title: "Notes from Underground ", author: "Dostoevsky");
+ Book b6=Book(title: "The Gambler ", author: "Dostoevsky");
+ Book b7=Book(title: "Poor Folk ", author: "Dostoevsky");
+ Book b8=Book(title: "White Nights ", author: "Dostoevsky");
+ Book b9=Book(title: "The Eternal Husband ", author: "Dostoevsky");
+ Book b10=Book(title: "The Insulted and Humiliated ", author: "Dostoevsky");
+
+  
 }
+
+class Book{
+  String? title;
+  String?author;
+  Book({required String title,required String author }){
+    this.title=title;
+    this.author=author;
+  }
+}*/
+
+
+/*
+///task2
+
+/*2.1.Define a BankAccount class with properties like id , expiredate , year ,cash
+2.2 Create 3 VIP clients in this bank (the cash of his/her acoount more than 1 milion)
+2.3 when call AccountDetails (method) return  that the account is vip or not ?*/
+void main(){
+  
+  BankAccount cl1=BankAccount(id: "1", expiredate: "12/2027", year: 30, cash: 1000000);
+  BankAccount cl2=BankAccount(id: "2", expiredate: "10/2030", year: 35, cash: 100000);
+  BankAccount cl3=BankAccount(id: "33", expiredate: "8/2035", year: 25, cash: 10000000);
+
+
+
+ print(cl1.AccountDetails());
+ print(cl2.AccountDetails());
+ print(cl3.AccountDetails());
+}
+///class
+class BankAccount {
+
+  ///properties
+  String? id;
+  String?expiredate;
+  int?year;
+  double?cash;
+
+  ///constructor
+  BankAccount({required this.id,required this.expiredate,required this.year,required this.cash});
+   
+
+   ///method
+   String AccountDetails ()
+   {
+    if (cash!>1000000)
+    {
+      return "The client is VIP";
+
+    }
+    else{
+      return"The client is Not VIP";
+    }
+   }
+   
+}
+*/
+
+/*
+
+/*task3
+. Extend the TrafficLight enum to store a duration for each light 
+ 3.2 Modify the TrafficLight enum and create a class TrafficSystem with a method getNextLight(TrafficLight current) that returns the next light based on the current one*/
+
+ void main(){
+  TrafficLight light=TrafficLight.red;
+   dynamic c =10;
+  while(true)
+  {
+  switch(light){
+    case TrafficLight.red:
+    print("Yellow");
+    light=TrafficLight.yellow;
+    break;
+    case TrafficLight.yellow:
+     print("Green");
+     light=TrafficLight.green;
+     break;
+    case TrafficLight.green:
+      print("Red");
+      light=TrafficLight.red;
+      break;
+      
+  }
+  c--;
+  if(c<=0)
+  {
+    break;
+  }
+  }
+
+ }
+
+enum TrafficLight {red,yellow,green}
+*/
